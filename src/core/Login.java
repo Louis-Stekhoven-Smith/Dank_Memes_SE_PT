6cec24f9370f6c66f7a18967f5e84b02eb089a6c
Louis <louis.stekhoven@gmail.com>
2017-03-10 03:00:17 +1100

    Branched, created login class and skelton tests functions.

---
+++ src/core/Login.java
@@ -0,0 +1,18 @@
+package core;
+
+/**
+ * Created by louie on 10/03/2017.
+ */
+public class Login {
+    private String test = "test";
+
+    public Login(){
+        this.test = "test";
+
+    }
+
+    public String getName(){
+        return test;
+    }
+
+}
---
+++ tests/core/asdf.java
@@ -0,0 +1,7 @@
+package core;
+
+/**
+ * Created by louie on 10/03/2017.
+ */
+public class asdf {
+}

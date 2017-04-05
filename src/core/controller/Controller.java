63783804a3ecbc619b133b78f45f813861fbaf2b
Hazza203 <s3383212@student.rmit.edu.au>
2017-04-05 20:50:42 +1000

    For pull request

--- src/core/controller/Controller.java
+++ src/core/controller/Controller.java
@@ -178,7 +178,7 @@ public class Controller {
         else if (result == 1){
             log.debug("Customer Logged in as: " + inputUsername);
             log.debug("Loading customer page...");
-            Parent LoginSuccess_parent = FXMLLoader.load(getClass().getResource("../view/LoginSuccess.fxml"));
+            Parent LoginSuccess_parent = FXMLLoader.load(getClass().getResource("../view/ChooseBusiness.fxml"));
             Scene LoginSuccess_scene = new Scene (LoginSuccess_parent);
             Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             primaryStage.close();

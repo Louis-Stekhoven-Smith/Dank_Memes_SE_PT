260f18c68f2373ec7e2970330bf6424b595f6212
Hazza203 <s3383212@student.rmit.edu.au>
2017-04-25 21:48:35 +1000

    Remove booking functionality moved into the view bookings page so you can select and delete an entry

--- src/main/java/core/model/Business.java
+++ src/main/java/core/model/Business.java
@@ -1,5 +1,11 @@
 package core.model;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by louie on 2/04/2017.
  */
@@ -10,6 +16,8 @@ public class Business {
     private String owner;
     private String logo;
     private Database database;
+    private ResultSet rs;
+    private static final Logger log = LogManager.getLogger(Business.class.getName());
 
     public Business(int businessID, Database database) {
         this. businessID = businessID;
@@ -33,6 +41,54 @@ public class Business {
 
     public String getLogo() {
         return logo;
+    }
+
+    /**Method for adding a new service to the database*/
+    public int addNewService(String name, int length){
+        log.debug("Adding new service to database");
+        //get businessID function here
+
+        //Validating name is alphabetic
+        if(!name.matches("[a-zA-z ]+") || name.equals("")){
+            return -1;
+        }
+        //Validating that the role is not already in the system
+        if(!roleExists(name)){
+            return -2;
+        }
+        //Validating length is in multiples of 15
+        if(length%15!=0){
+            log.debug("Failed to add service, incorrectly specified length");
+            return -3;
+        }
+
+        String addServiceSQL = "INSERT into availableServices(serviceID, businessID, serviceName, serviceLength) values(?, " +
+                "'" + 1 + "'," +
+                "'" + name + "'," +
+                "'" + length + "')";
+
+        if(!database.updateDatabase(addServiceSQL)){
+            return 0;
+        }
+
+        return 1;
+
+    }
+
+    /**Method to make sure that the role is not already in the system */
+    public boolean roleExists(String name){
+        String findRoleSQL = "SELECT serviceName FROM availableServices WHERE businessID =" +
+                "'" + 1 + "' AND serviceName ='" + name + "' COLLATE NOCASE";
+
+        rs = database.queryDatabase(findRoleSQL);
+        try{
+            if(rs.next()){
+                return false;
+            }
+        } catch(SQLException e){
+            log.debug("SQL ERROR: " + e.getMessage());
+        }
+        return true;
     }
 
 

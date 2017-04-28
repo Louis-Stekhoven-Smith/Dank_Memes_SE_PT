887cc606eb4d2f9f075e0cd15fe3b50ba685033e
rmit-s3512341-phi-nguyen <s3512341@student.rmit.edu.au>
2017-04-29 05:56:08 +1000

    Removal of buttons, combo box added to show bookings after remembering the number constraint with buttons. 6 Straight hours trying to figure everything out, want to kill myself

--- src/main/java/core/controller/CurrentBookingsController.java
+++ src/main/java/core/controller/CurrentBookingsController.java
@@ -9,8 +9,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.Array;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -41,7 +44,42 @@ public class CurrentBookingsController {
     @FXML
     private Button btnBooking5;
 
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Label lblBookingID;
+
+    @FXML
+    private Label lblBusinessName;
+
+    @FXML
+    private Label lblEmployeeName;
+
+    @FXML
+    private Label lblYourName;
+
+    @FXML
+    private Label lblBookingType;
+
+    @FXML
+    private Label lblBookingTime;
+
+    @FXML
+    private Label lblBookingDate;
+
+    @FXML
+    private ComboBox<String> bookingsCombo;
+
     public static int userLoggedID = 1;
+    public static String currentBooking = "1";
+    public static String businessName ="";
+    public static String employeeName ="";
+    public static String yourName ="";
+    public static String bookingType ="";
+    public static String bookingTime ="";
+    public static String bookingDate ="";
+    public static String[][] myArray;
 
     private Database database = Database.getInstance();
 
@@ -68,56 +106,59 @@ public class CurrentBookingsController {
                 row[i] = result.getString(i + 1);
             }
             rs.add(row);
-        }
+        } System.out.println(counter);
 
         String[][] newArray = new String[counter][counter];
 
-        for (int i=0; i<counter; i++) {
+        for (int i=0; i<newArray.length; i++) {
             newArray[i] = rs.get(i);
+            bookingsCombo.getItems().addAll(String.valueOf(i+1));
             System.out.println(Arrays.toString(newArray[i]));
-        }
-
-        if (counter == 1){
-            btnBooking1.setVisible(true);
-            btnBooking1.setText("Booking No. "+ newArray[0][0]);
-        } else if (counter == 2){
-            btnBooking1.setVisible(true);
-            btnBooking2.setVisible(true);
-            btnBooking1.setText("Booking No. "+ newArray[0][0]);
-            btnBooking2.setText("Booking No. "+ newArray[1][0]);
-        } else if (counter == 3){
-            btnBooking1.setVisible(true);
-            btnBooking2.setVisible(true);
-            btnBooking3.setVisible(true);
-            btnBooking1.setText("Booking No. "+ newArray[0][0]);
-            btnBooking2.setText("Booking No. "+ newArray[1][0]);
-            btnBooking3.setText("Booking No. "+ newArray[2][0]);
-        } else if (counter == 4){
-            btnBooking1.setVisible(true);
-            btnBooking2.setVisible(true);
-            btnBooking3.setVisible(true);
-            btnBooking4.setVisible(true);
-            btnBooking1.setText("Booking No. "+ newArray[0][0]);
-            btnBooking2.setText("Booking No. "+ newArray[1][0]);
-            btnBooking3.setText("Booking No. "+ newArray[2][0]);
-            btnBooking4.setText("Booking No. "+ newArray[3][0]);
-        } else if (counter == 5){
-            btnBooking1.setVisible(true);
-            btnBooking2.setVisible(true);
-            btnBooking3.setVisible(true);
-            btnBooking4.setVisible(true);
-            btnBooking5.setVisible(true);
-            btnBooking1.setText("Booking No. "+ newArray[0][0]);
-            btnBooking2.setText("Booking No. "+ newArray[1][0]);
-            btnBooking3.setText("Booking No. "+ newArray[2][0]);
-            btnBooking4.setText("Booking No. "+ newArray[3][0]);
-            btnBooking5.setText("Booking No. "+ newArray[4][0]);
-        }
+        } myArray = newArray;
     }
 
+    public void queries(ActionEvent event, String custName, String businessID, String empNo) throws IOException, SQLException{
 
-    
+        ResultSet rs2;
+        String custN;
+        String findcustNSQL = "SELECT name FROM customerDetails WHERE custID=" + "'" + custName + "'";
+        rs2 = database.queryDatabase(findcustNSQL);
+        custN = rs2.getString("name");
 
+        ResultSet rs;
+        String businessN;
+        String findBusinessNSQL = "SELECT businessName FROM businessDetails WHERE businessID=" + "'" + businessID + "'";
+        rs = database.queryDatabase(findBusinessNSQL);
+        businessN = rs.getString("businessName");
+
+
+        ResultSet rs1;
+        String empN;
+        String findempNSQL = "SELECT name FROM employeeDetails WHERE empID=" + "'" + empNo + "'";
+        rs1 = database.queryDatabase(findempNSQL);
+        empN = rs1.getString("name");
+
+        businessName = businessN;
+        employeeName = empN;
+        yourName = custN;
+    }
+
+    public void comboChanged (ActionEvent event) throws IOException, SQLException {
+        lblBookingID.setText(myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][0]);
+
+        String custName = myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][1];
+        String businessID = myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][2];
+        String empNo = myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][3];
+
+        queries(event, custName, businessID, empNo);
+
+        lblBusinessName.setText(businessName);
+        lblEmployeeName.setText(employeeName);
+        lblYourName.setText(yourName);
+        lblBookingType.setText(myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][6]);
+        lblBookingDate.setText(myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][5]);
+        lblBookingTime.setText(myArray[(Integer.valueOf(bookingsCombo.getValue())-1)][4]);
+    }
 
     public void btnBackPressed (ActionEvent event) throws IOException {
         Parent Back_parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/CustomerNavigation.fxml"));

e95f4c498b590169483425358102ef8300a08101
rmit-s3512341-phi-nguyen <s3512341@student.rmit.edu.au>
2017-04-28 22:57:16 +1000

    Customer Navigation page UI, back button and make booking button works

--- src/main/java/core/controller/CustomerNavigationController.java
+++ src/main/java/core/controller/CustomerNavigationController.java
@@ -1,7 +1,46 @@
 package core.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 /**
  * Created by Konn on 28/04/2017.
  */
+
 public class CustomerNavigationController {
+
+    @FXML
+    private Button btnCustNav1;
+
+    @FXML
+    private Button btnCustNav2;
+
+    @FXML
+    private Button btnBack;
+
+    public void btnCustNav1Pressed (ActionEvent event) throws IOException {
+        Parent LoginSuccess_parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/ChooseBusiness.fxml"));
+        Scene LoginSuccess_scene = new Scene (LoginSuccess_parent);
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.close();
+        primaryStage.setScene(LoginSuccess_scene);
+        primaryStage.show();
+    }
+
+    public void btnBackPressed (ActionEvent event) throws IOException {
+        Parent LoginSuccess_parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/LoginPage.fxml"));
+        Scene LoginSuccess_scene = new Scene (LoginSuccess_parent);
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.close();
+        primaryStage.setScene(LoginSuccess_scene);
+        primaryStage.show();
+    }
+
 }

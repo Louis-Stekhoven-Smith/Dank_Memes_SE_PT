93f08b80159d67622788d8d34aced885c7005dcf
rmit-s3512341-phi-nguyen <s3512341@student.rmit.edu.au>
2017-05-10 17:58:06 +1000

    Choose business page updated to be dynamic, you now choose business from combobox (loads businesses from database) and then click on the business button. It uses single button now so other "btnBusiness2Pressed" etc are non-functional. This is because it now updates the variable to the business rather than choosing the business via its own assigned button which made dynamically loaded businesses/buttons not possible. The button also now loads its background from the businessDetails database page since it is now dynamic.
    PROBLEM: Keeps inserting the same data into the businessDetails database page and the userLogin database page. It may be due to my edits in the config file or the Database class regarding the availability (mon,tue,wed,thu,fri,sat,sun), unsure.

--- src/main/java/core/controller/ChooseBusinessController.java
+++ src/main/java/core/controller/ChooseBusinessController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
@@ -25,6 +26,9 @@ import java.util.Arrays;
 
 public class ChooseBusinessController {
 
+    public int businessSelector = 0;
+
+    @FXML private Button btnBusiness1;
     @FXML private ComboBox<String> businessChooser;
 
     private Database database = Database.getInstance();
@@ -49,14 +53,35 @@ public class ChooseBusinessController {
             businessChooser.getItems().addAll(String.valueOf(myArray[i]));
         }
 
+    }
 
+          public void comboChanged() throws IOException, SQLException {
+        btnBusiness1.setVisible(true);
+
+        ResultSet business;
+        int businessNo;
+        String findBusinessNo = "SELECT businessID FROM businessDetails WHERE businessName = '" + businessChooser.getValue() + "'";
+        business = database.queryDatabase(findBusinessNo);
+        businessNo = business.getInt("businessID");
+        businessSelector = businessNo;
+        System.out.println(businessSelector);
+
+        ResultSet businessBG;
+        String businessBackground;
+        String findBusinessBG = "SELECT bg FROM businessDetails WHERE businessName = '" + businessChooser.getValue() + "'";
+        businessBG = database.queryDatabase(findBusinessBG);
+        businessBackground = businessBG.getString("bg");
+
+
+        btnBusiness1.setStyle("-fx-background-image: url('"+businessBackground+"')");
+        btnBusiness1.setVisible(true);
     }
 
     Session session = Session.getInstance();
     /*TODO  hard code a bunch business buttons, hide them if a business matching that id doesn't yet exist*/
     @FXML
     public void btnBusiness1Clicked(javafx.event.ActionEvent event) throws IOException {
-        session.setBusinessSelected(1);
+        session.setBusinessSelected(businessSelector);
         selectedBusiness(event);
     }
 
